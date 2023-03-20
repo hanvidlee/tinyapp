7260cafdde4399d83ae2e575140a87390b25d4d5
hanvidlee <hanvidlee@gmail.com>
2023-03-20 19:31:40 -0400

    installed ejs

--- express_server.js
+++ express_server.js
@@ -1,14 +1,17 @@
-const express = require("express")
-const app = express()
-const PORT = 8080
+const express = require("express");
+const app = express();
+const PORT = 8080;
 
-const urlDatabase = {  "b2xVn2": "http://www.lighthouselabs.ca",
-"9sm5xK": "http://www.google.com"
-}
+app.set("view engine", "ejs");
 
-app.get("/",(req,res) =>{
-  res.send("Hello!")
-})
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello!");
+});
 
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
@@ -19,4 +22,5 @@ app.get("/hello", (req, res) => {
 });
 
 app.listen(PORT, () => {
-console.log(`Example app listening on port ${PORT}`)})
+  console.log(`Example app listening on port ${PORT}`);
+});
